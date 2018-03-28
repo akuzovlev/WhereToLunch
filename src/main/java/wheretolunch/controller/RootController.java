7a840551aa878146c66baa096b31f6bb1b7d4abc
Alexander Kuzovlev <basistadom@gmail.com>
2018-03-28 22:10:20 +0300

    Added project skeleton

---
+++ src/main/java/wheretolunch/controller/RootController.java
@@ -0,0 +1,19 @@
+package wheretolunch.controller;
+
+        import org.springframework.web.servlet.ModelAndView;
+        import org.springframework.web.servlet.mvc.AbstractController;
+
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
+
+public class RootController extends AbstractController {
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+                                                 HttpServletResponse response) throws Exception {
+
+        ModelAndView model = new ModelAndView("login");
+
+        return model;
+    }
+}
